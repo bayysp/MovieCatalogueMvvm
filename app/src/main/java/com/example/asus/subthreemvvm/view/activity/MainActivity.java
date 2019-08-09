38cb88f2ac4fa4cc6bbd002d7aa32f674eb57f77
Bayu Saputra <babahayoo@gmail.com>
2019-08-09 19:35:04 +0700

    set searchview success using viewodel, but still cannot show in fragment

--- app/src/main/java/com/example/asus/subthreemvvm/view/activity/MainActivity.java
+++ app/src/main/java/com/example/asus/subthreemvvm/view/activity/MainActivity.java
@@ -1,19 +1,31 @@
 package com.example.asus.subthreemvvm.view.activity;
 
+import android.app.SearchManager;
+import android.arch.lifecycle.Observer;
+import android.arch.lifecycle.ViewModelProviders;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.provider.Settings;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.SearchView;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.example.asus.subthreemvvm.R;
+import com.example.asus.subthreemvvm.model.MovieItem;
 import com.example.asus.subthreemvvm.view.fragment.FavoriteFragment;
 import com.example.asus.subthreemvvm.view.fragment.MovieFragment;
 import com.example.asus.subthreemvvm.view.fragment.TvshowFragment;
+import com.example.asus.subthreemvvm.viewmodel.SearchMovieViewModel;
+
+import java.util.ArrayList;
 
 import static com.example.asus.subthreemvvm.base.BaseAppCompatActivity.KEY_FRAGMENT;
 import static com.example.asus.subthreemvvm.base.BaseAppCompatActivity.KEY_TITLE;
@@ -22,9 +34,11 @@ public class MainActivity extends AppCompatActivity implements BottomNavigationV
 
     BottomNavigationView bottomNav;
 
-    Fragment selectedFragment= new MovieFragment();
+    Fragment selectedFragment = new MovieFragment();
 
     String title = "Home";
+
+    private SearchMovieViewModel searchMovieViewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,11 +48,11 @@ public class MainActivity extends AppCompatActivity implements BottomNavigationV
         bottomNav = findViewById(R.id.bottom_nav_menu);
         bottomNav.setOnNavigationItemSelectedListener(this);
 
-        if (savedInstanceState == null){
+        if (savedInstanceState == null) {
             loadFragment(selectedFragment);
             setTitle(title);
-        }else{
-            selectedFragment = getSupportFragmentManager().getFragment(savedInstanceState,KEY_FRAGMENT);
+        } else {
+            selectedFragment = getSupportFragmentManager().getFragment(savedInstanceState, KEY_FRAGMENT);
             title = savedInstanceState.getString(KEY_TITLE);
             loadFragment(selectedFragment);
             setTitle(title);
@@ -46,12 +60,12 @@ public class MainActivity extends AppCompatActivity implements BottomNavigationV
     }
 
     private boolean loadFragment(Fragment fragment) {
-        if (fragment != null){
+        if (fragment != null) {
             getSupportFragmentManager()
                     .beginTransaction()
-                    .replace(R.id.fragment_container,fragment)
+                    .replace(R.id.fragment_container, fragment)
                     .commit();
-
+            Log.d("MainActivityOptions", "selectedFragment is : "+selectedFragment);
             return true;
         }
         return false;
@@ -59,9 +73,10 @@ public class MainActivity extends AppCompatActivity implements BottomNavigationV
 
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
-        switch (menuItem.getItemId()){
+        switch (menuItem.getItemId()) {
             case R.id.menu_bottomnav_movie:
                 selectedFragment = new MovieFragment();
+
                 break;
 
             case R.id.menu_bottomnav_tvshows:
@@ -77,13 +92,48 @@ public class MainActivity extends AppCompatActivity implements BottomNavigationV
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_settings,menu);
+        getMenuInflater().inflate(R.menu.menu_settings, menu);
+
+        SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+
+        if (searchManager != null){
+            SearchView searchView = (SearchView) menu.findItem(R.id.search_menu).getActionView();
+            searchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
+            searchView.setQueryHint(getResources().getString(R.string.input_text));
+            searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+                @Override
+                public boolean onQueryTextSubmit(String s) {
+                    Log.d("MainActivity","OptionsMenu - onQueryTextSubmit");
+                    try {
+                        searchData(s);
+                        return true;
+                    }catch (Exception e){
+                        Log.d("MainActivity","OptionsMenu - onQueryTextSubmit - Error");
+                        Toast.makeText(getApplicationContext(),"Data Tidak Ditemukan",Toast.LENGTH_SHORT).show();
+                        return false;
+                    }
+                }
+
+                @Override
+                public boolean onQueryTextChange(String s) {
+                    Log.d("MainActivity","OptionsMenu - onQueryTextChange");
+                    try {
+                        searchData(s);
+                        return true;
+                    }catch (Exception e){
+                        Log.d("MainActivity","OptionsMenu - onQueryTextChange - Error");
+                        Toast.makeText(getApplicationContext(),"Data Tidak Ditemukan",Toast.LENGTH_SHORT).show();
+                        return false;
+                    }
+                }
+            });
+        }
         return super.onCreateOptionsMenu(menu);
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        if (item.getItemId() == R.id.action_change_settings){
+        if (item.getItemId() == R.id.action_change_settings) {
             Intent mIntent = new Intent(Settings.ACTION_LOCALE_SETTINGS);
             startActivity(mIntent);
         }
@@ -93,8 +143,27 @@ public class MainActivity extends AppCompatActivity implements BottomNavigationV
     @Override
     protected void onSaveInstanceState(Bundle outState) {
         super.onSaveInstanceState(outState);
-        outState.putString(KEY_TITLE,title);
+        outState.putString(KEY_TITLE, title);
 
-        getSupportFragmentManager().putFragment(outState,KEY_FRAGMENT,selectedFragment);
+        getSupportFragmentManager().putFragment(outState, KEY_FRAGMENT, selectedFragment);
     }
+
+    private void searchData(String keyword){
+        if (selectedFragment instanceof MovieFragment) {
+            searchMovieViewModel = ViewModelProviders.of(this).get(SearchMovieViewModel.class);
+            searchMovieViewModel.setMovieSearch(getResources().getString(R.string.code_language),keyword);
+            searchMovieViewModel.getSearchMovies().observe(this,getSearchMovie);
+        } else {
+
+        }
+    }
+
+    private Observer<ArrayList<MovieItem>> getSearchMovie = new Observer<ArrayList<MovieItem>>() {
+        @Override
+        public void onChanged(@Nullable ArrayList<MovieItem> movieItems) {
+            if (movieItems != null){
+                Log.d("GetSearchMovieData",String.valueOf(movieItems));
+            }
+        }
+    };
 }
--- app/src/main/java/com/example/asus/subthreemvvm/viewmodel/SearchMovieViewModel.java
+++ app/src/main/java/com/example/asus/subthreemvvm/viewmodel/SearchMovieViewModel.java
@@ -15,39 +15,37 @@ import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 
-public class MovieViewModel extends ViewModel {
-
+public class SearchMovieViewModel extends ViewModel {
     private MovieService movieService;
     private String lang;
+    private String movieName;
 
-    private MutableLiveData<ArrayList<MovieItem>> listMovie = new MutableLiveData<>();
+    private MutableLiveData<ArrayList<MovieItem>> listMovieSearch = new MutableLiveData<>();
 
-
-    public void setMovie(String lang){
+    public void setMovieSearch(String lang,String movieName){
         this.lang = lang;
+        this.movieName = movieName;
         if (this.movieService == null){
             movieService = new MovieService();
         }
 
-        movieService.getMovieApi().getMovie(lang).enqueue(new Callback<MovieResponse>() {
+        movieService.getMovieApi().getSearchMovie(lang,movieName).enqueue(new Callback<MovieResponse>() {
             @Override
             public void onResponse(Call<MovieResponse> call, Response<MovieResponse> response) {
                 MovieResponse movieResponse = response.body();
                 if (movieResponse != null && movieResponse.getResults() != null){
                     ArrayList<MovieItem> movieItems = movieResponse.getResults();
-                    Log.d("MovieViewModel","onResponse success" + movieItems);
-                    listMovie.postValue(movieItems);
+                    Log.d("SearchMovieViewModel","onResponse success" + movieItems);
+                    listMovieSearch.postValue(movieItems);
                 }
             }
 
             @Override
             public void onFailure(Call<MovieResponse> call, Throwable t) {
-                Log.d("MovieViewModel","onFailure " + t.getMessage());
+                Log.d("SearchMovieViewModel","onFailure " + t.getMessage());
             }
         });
     }
 
-    public LiveData<ArrayList<MovieItem>> getMovies(){
-        return listMovie;
-    }
+    public LiveData<ArrayList<MovieItem>> getSearchMovies(){return listMovieSearch;}
 }
